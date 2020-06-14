9eb6b674cdd855679e608c41e9008ce088369e3b
Mike Rock <mr392@njit.edu>
2020-06-13 22:46:26 -0400

    Feature: Added text message

--- src/my_script.py
+++ src/my_script.py
@@ -5,5 +5,8 @@ init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
 from termcolor import cprint
 from pyfiglet import figlet_format
 
-cprint(figlet_format('Hello!', font='starwars'),
-       'blue', 'on_white', attrs=['bold'])
+cprint(figlet_format('Hello! ', font='banner'),
+       'red', 'on_yellow')
+
+cprint(figlet_format('This works!', font='banner'),
+       'red', 'on_yellow')
